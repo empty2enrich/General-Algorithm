4b61e7c8fe5cf3a00012121065570c9a3ae0b1ab
emptyknowledge <2056374813@qq.com>
2017-08-15 18:25:40 +0800

    function

--- SortAlgorithm/NaturalSort/NaturalSort.java
+++ SortAlgorithm/NaturalSort/NaturalSort.java
@@ -1,1 +1,28 @@
 
+public class NaturalSort {
+
+  public int[] nums;
+
+  /**
+  *
+  * NaturalSort
+  *
+  */
+  public void sort(){
+
+  }
+
+  /**
+  *
+  * getSortInfo
+  *
+  */
+  public void getSortInfo(){
+
+  }
+
+  public void merge(){
+    
+  }
+
+}
